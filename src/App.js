4e3e318dc140fed07e84b8d3510d09bc1d225102
pjmantoss <joelptoss@gmail.com>
2020-05-03 17:14:16 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -84,6 +84,11 @@ function App() {
             </div>
           </div>
         ) : ('')}
+        <div id="love">
+            <span>Made With</span>
+            <img src="https://img.icons8.com/dusk/64/000000/like.png" alt="love"/> 
+            <span>by <a href="https://github.com/PJMantoss" target="_blank">Mantoss</a></span>
+        </div>
       </main>
     </div>
   );
