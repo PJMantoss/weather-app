ded9c239a928d096553b28b2a21efe94408ff75c
pjmantoss <joelptoss@gmail.com>
2020-05-03 17:25:46 +0100

    update styling

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ function App() {
         <div id="love">
             <span>Made With</span>
             <img src="https://img.icons8.com/dusk/64/000000/like.png" alt="love"/> 
-            <span>by <a href="https://github.com/PJMantoss" target="_blank">Mantoss</a></span>
+            <span>by <a href="https://github.com/PJMantoss" target="_blank" rel="noopener noreferrer">Mantoss</a></span>
         </div>
       </main>
     </div>
