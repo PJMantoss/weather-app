2a3a0949601dfd7e859934e9933cc5a16a9dd7c7
pjmantoss <joelptoss@gmail.com>
2020-05-03 14:52:20 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import './App.css';
 
+const api = {
+  key: "2d174703d6e4d8d4645eeba543dbd48c",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 function App() {
   return (
     <div className="App">
